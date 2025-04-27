754b8b627ff43f57d384ab36274d4ab98d99f3a9
Cristian Marcial Olivera Hidalgo <oliverason2603@gmail.com>
2025-04-27 18:13:18 +0200

    Primeros pasos creando la web

---
+++ script.js
@@ -0,0 +1,77 @@
+document.getElementById('download-cv').addEventListener('click', function() {
+    window.open('cv.pdf', '_blank');
+  });
+
+
+// Toggle modo claro / oscuro
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', function() {
+    
+    document.body.classList.toggle('light-mode');
+  
+  if (document.body.classList.contains('light-mode')) {
+    themeToggle.textContent = "🌞"; // Modo claro: muestra sol
+    checkThemeAndToggleClouds();
+  } else {
+    themeToggle.textContent = "🌙"; // Modo oscuro: muestra luna
+  }
+});
+
+
+// Configuración de particles.js
+particlesJS('particles-js', {
+    particles: {
+      number: {
+        value: 60,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: { value: "#ffffff" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#000000" },
+        polygon: { nb_sides: 5 }
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: { enable: false }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: { enable: false }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 3,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "repulse" },
+        onclick: { enable: true, mode: "push" }
+      },
+      modes: {
+        repulse: { distance: 100, duration: 0.4 },
+        push: { particles_nb: 4 }
+      }
+    },
+    retina_detect: true
+  });  
+
